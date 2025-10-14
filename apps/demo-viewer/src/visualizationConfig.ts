0f224aac5c47239b90fce63729e1bd3d5bf383ff
Tobias S <tobio@lunit.io>
2025-10-14 17:51:18 +0900

    feat: adjust colours for new annotations

--- apps/demo-viewer/src/visualizationConfig.ts
+++ apps/demo-viewer/src/visualizationConfig.ts
@@ -1,24 +1,20 @@
 export const visualizationConfig = {
   pixelLayers: [
     {
-      label: "Planitia Areas",
+      label: "Terra Areas",
       color: "#5f9b1c",
     },
     {
-      label: "Planum Areas",
+      label: "Planitia Areas",
       color: "#7f4883",
     },
     {
-      label: "Terra Areas",
+      label: "Planum Areas",
       color: "#f76707",
     },
     {
-      label: "Chaos (Channels)",
+      label: "Chaos and Channels",
       color: "#84acba",
-    },
-    {
-      label: "Craters",
-      color: "#d8cd01",
     },
     {
       label: "Mountains",
