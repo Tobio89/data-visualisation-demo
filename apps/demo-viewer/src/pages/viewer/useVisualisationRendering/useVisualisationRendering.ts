e37f4137dc73bef5ef534f9c1a2a7bb74f476f12
Tobias S <tobio@lunit.io>
2025-10-14 19:07:34 +0900

    feat: render points at mars mountain data

--- apps/demo-viewer/src/pages/viewer/useVisualisationRendering/useVisualisationRendering.ts
+++ apps/demo-viewer/src/pages/viewer/useVisualisationRendering/useVisualisationRendering.ts
@@ -1,13 +1,33 @@
 import { useCallback } from "react";
 
+import marsLocaleData from "../../../marsLocaleData.json";
+
 import type { TooltipOverlayRedrawArgs } from "./useVisualisationRendering.types";
 
 const useVisualisationRendering = () => {
+  const { mountains } = marsLocaleData;
+
   const onTooltipOverlayRedraw = useCallback(
-    ({ tooltipCoord, originalEvent }: TooltipOverlayRedrawArgs) => {
+    ({
+      tooltipCoord,
+      originalEvent,
+      overlayCanvasEl,
+    }: TooltipOverlayRedrawArgs) => {
       if (originalEvent?.ctrlKey) {
         console.log("mouse at: ", tooltipCoord);
       }
+
+      const ctx = overlayCanvasEl.getContext("2d");
+      if (!ctx) return;
+
+      mountains.forEach((mountain) => {
+        const { x, y } = mountain.point;
+        ctx.fillStyle = "#fff";
+        ctx.beginPath();
+        ctx.ellipse(x, y, 12, 12, 0, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+      });
     },
     []
   );
