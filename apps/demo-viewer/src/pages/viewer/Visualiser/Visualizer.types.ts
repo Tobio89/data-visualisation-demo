9dfc6a160d1874c1418291c31efef0da6d7d9a4d
Tobias S <tobio@lunit.io>
2025-10-15 14:58:19 +0900

    feat: add DeckGL without causing any breaches to the fabric of space-time

--- apps/demo-viewer/src/pages/viewer/Visualiser/Visualizer.types.ts
+++ apps/demo-viewer/src/pages/viewer/Visualiser/Visualizer.types.ts
@@ -1,5 +1,7 @@
+import type { Deck } from "@deck.gl/core";
 import type { TooltipOverlayRedrawArgs } from "../useVisualisationRendering/useVisualisationRendering.types";
 
 export interface VisualizerProps {
   onTooltipOverlayRedraw: (event: TooltipOverlayRedrawArgs) => void;
+  onDeckGLOverlayRedraw: (viewer: OpenSeadragon.Viewer, deck: Deck) => void;
 }
